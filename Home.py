2fc33492a50d28d21b97178af6518ed5dcdb8f59
Juan Cruz Saldaño <70445613+fowardelcac@users.noreply.github.com>
2023-06-21 16:33:23 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -21,7 +21,7 @@ def main():
     st.markdown('''
                 1. Analizador de Ethereum Addresses: Este proyecto permite ingresar una dirección de Ethereum y obtener información relevante sobre sus transacciones, tokens ERC20 y NFTs asociados. Proporciona una visión detallada de la actividad y los activos digitales relacionados con una dirección específica en la blockchain de Ethereum.
                 2. Optimizacion de portafolio: En este proyecto, se enfoca en la optimización de portafolios utilizando la frontera eficiente de Markowitz y el índice de Sharpe. Se analizan tres activos: MSFT, AAPL y TSLA, para obtener los pesos y valores óptimos del portafolio. El objetivo es maximizar el rendimiento y reducir el riesgo de la cartera de inversiones.
-                3. First payment default: Este proyecto se centra en el análisis de riesgo crediticio utilizando un dataset sintético y un modelo de machine learning llamado LGBM. Los usuarios pueden ingresar sus datos en la página y el modelo determinará si son aptos para obtener una tarjeta de crédito o no.
+                3. Aprobacion tarjeta de credito: Este proyecto se centra en el análisis de riesgo crediticio utilizando un dataset sintético y un modelo de machine learning llamado LGBM. Los usuarios pueden ingresar sus datos en la página y el modelo determinará si son aptos para obtener una tarjeta de crédito o no.
                 ''')
     
     st.subheader("Como contactarme:")
