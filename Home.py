6401c6a164e746e682589e3626b5c8f8123e799c
Juan Cruz Saldaño <70445613+fowardelcac@users.noreply.github.com>
2023-06-21 16:29:33 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -17,12 +17,12 @@ def main():
     st.markdown('''Soy Juan Cruz, estudiante de Licenciatura en Ciencia de Datos. Me apasiona la programación en Python y tengo experiencia en el desarrollo de aplicaciones web interactivas con Streamlit. Trabajo con librerías como Pandas, Matplotlib/Seaborn, Numpy, Sklearn y Yfinance para análisis de datos y Machine Learning. Busco oportunidades laborales en el campo de la ciencia de datos, con interés en el sector financiero y tecnológico. Soy autodidacta, tengo nivel intermedio de inglés y estoy ansioso por colaborar en proyectos desafiantes. ¡Gracias por tu consideración!''')    
     
     st.subheader('Descripcion:')
-    st.markdown('''Este es mi portafolio de Ciencia de Datos, el cual consta de tres proyectos destacados.
+    st.markdown("Este es mi portafolio de Ciencia de Datos, el cual consta de tres proyectos destacados:"
+    st.markdown('''
                 1. Analizador de Ethereum Addresses: Este proyecto permite ingresar una dirección de Ethereum y obtener información relevante sobre sus transacciones, tokens ERC20 y NFTs asociados. Proporciona una visión detallada de la actividad y los activos digitales relacionados con una dirección específica en la blockchain de Ethereum.
                 2. Optimizacion de portafolio: En este proyecto, se enfoca en la optimización de portafolios utilizando la frontera eficiente de Markowitz y el índice de Sharpe. Se analizan tres activos: MSFT, AAPL y TSLA, para obtener los pesos y valores óptimos del portafolio. El objetivo es maximizar el rendimiento y reducir el riesgo de la cartera de inversiones.
                 3. First payment default: Este proyecto se centra en el análisis de riesgo crediticio utilizando un dataset sintético y un modelo de machine learning llamado LGBM. Los usuarios pueden ingresar sus datos en la página y el modelo determinará si son aptos para obtener una tarjeta de crédito o no.
-    
-    .''')
+                ''')
     
     st.subheader("Como contactarme:")
     st.markdown('''
