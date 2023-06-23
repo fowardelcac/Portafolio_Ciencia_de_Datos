5a8f3e284a965a4957d5eae6d1927ea69bb22d2f
Juan Cruz Saldaño <70445613+fowardelcac@users.noreply.github.com>
2023-06-22 23:29:54 -0300

    Update 2_Optimizacion_de_portafolio.py

--- pages/2_Optimizacion_de_portafolio.py
+++ pages/2_Optimizacion_de_portafolio.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-import scipy.optimize as sci_opt
+from scipy import optimize
 
 st.set_page_config(
     page_title="Portfolio",
