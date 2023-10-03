f8adf97b0d0e0ba1ff003fea572789e40cde27b5
Juan Cruz Saldaño <70445613+fowardelcac@users.noreply.github.com>
2023-10-03 02:02:35 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,29 +11,33 @@ st.set_page_config(
 
 
 def main():
-    st.title("Bienvenidos a mi portafolio.")
-  
+   st.title("¡Bienvenidos a mi Portafolio!")
 
-    st.markdown('''Soy Juan Cruz, estudiante de Licenciatura en Ciencia de Datos. Cuento con conocimientos de programación tanto en Python como R; especificamente en python trabajo con librerías como Pandas, Matplotlib/Seaborn, Numpy, Sklearn y Machine Learning. Busco oportunidades laborales en el campo de la ciencia de datos, con interés en el sector tecnológico. Soy autodidacta, cuento nivel intermedio de inglés y estoy ansioso por colaborar en proyectos desafiantes. ¡Gracias por tu consideración!''')    
-    
-    st.subheader('Descripcion:')
-    st.markdown("Este es mi portafolio de Ciencia de Datos, el cual consta de algunos proyectos destacados:")
     st.markdown('''
-                Contenidos
-PBI dashboard: Este es un tablero de control creado en Python que presenta gráficos simples relacionados con el Producto Interno Bruto (PIB) a nivel mundial y regional. El objetivo principal de este proyecto es demostrar cómo se puede crear un tablero de control simple, claro y conciso utilizando Python y generar visualizaciones a través de Plotly. Su repositorio original: https://github.com/fowardelcac/PBI
-Analisis de sentimiento: El objetivo principal de este proyecto fue realizar un análisis de sentimiento en relación a cinco prominentes políticos argentinos. Este análisis se basó en la evaluación de tweets en los cuales los usuarios los mencionaban. La primera fase del proyecto involucró la extracción de tweets, específicamente aquellos publicados los días 13 y 14 de abril. Utilicé la biblioteca Snscrape para llevar a cabo esta tarea de manera eficiente y precisa. En la segunda etapa, trabajé en el procesamiento de los datos recolectados. Utilicé un modelo de Procesamiento de Lenguaje Natural (NLP) conocido como roBERTa, el cual me permitió llevar a cabo un análisis de sentimiento. Este análisis generó tres posibles puntajes: 1) Negativo, 2) Neutro y 3) Positivo, proporcionando una visión completa de las opiniones expresadas en los tweets. Finalmente, con base en los resultados obtenidos, elaboré conclusiones que contribuyeron a una comprensión más profunda de la percepción pública en torno a estos políticos. Este proyecto no solo demostró mis habilidades analíticas y mi capacidad para trabajar con herramientas avanzadas de procesamiento de datos, sino también mi capacidad para llevar a cabo investigaciones significativas y generar información valiosa a partir de datos no estructurados. Su repositorio original: https://github.com/fowardelcac/Analisis-de-sentimiento-politicos-argentinos
-Análisis de Direcciones de Ethereum: En este proyecto, me adentré en el emocionante mundo de las criptomonedas y me conecté a la API de Etherscan para crear una aplicación interactiva. Esta aplicación permite interactuar con la API para obtener datos a partir de las direcciones de Ethereum. Entre los datos disponibles se encuentran el saldo de Ethereum de cada dirección, el historial de transacciones y los tokens que poseen.vEl propósito principal de este proyecto es demostrar mi habilidad para acceder a datos utilizando una API y organizarlos de manera que proporcionen información valiosa y conocimientos significativos. A través de esta aplicación, no solo he obtenido datos detallados sobre las direcciones de Ethereum, sino que también he logrado visualizar insights importantes que pueden ser de utilidad tanto para investigadores como para entusiastas de las criptomonedas. Explorando las finanzas descentralizadas, la tecnología blockchain y el fascinante mundo de Ethereum, este proyecto es un testimonio de mi capacidad para trabajar con datos complejos y presentarlos de manera clara y efectiva para la toma de decisiones informadas. Repositorio original: https://github.com/fowardelcac/Analisis-ethereum-address
-                ''')
+    Soy Juan Cruz, un apasionado estudiante de Licenciatura en Ciencia de Datos. Mi enfoque se centra en la programación, tanto en Python como en R, donde destaco en el manejo de librerías como Pandas, Matplotlib/Seaborn, Numpy, Sklearn y Machine Learning. Estoy en búsqueda de oportunidades laborales en el apasionante campo de la Ciencia de Datos, con especial interés en el sector tecnológico.
     
-    st.subheader("Como contactarme:")
+    Soy autodidacta y cuento con un nivel intermedio de inglés. Estoy ansioso por colaborar en proyectos desafiantes y seguir aprendiendo en este emocionante camino. ¡Gracias por tu consideración!
+    ''')
+    
+    st.subheader('Descripción:')
+    st.markdown("Mi Portafolio de Ciencia de Datos presenta una variedad de proyectos destacados que incluyen:")
+    
     st.markdown('''
-                1. Linkedin: https://www.linkedin.com/in/jcs2
-                2. Github: https://github.com/fowardelcac
-                3. Gmail: juansaldano01@gmail.com
-                4. WhatsApp: 3515126823
-                ''')
+    - **PBI Dashboard**: Un tablero de control creado en Python que presenta gráficos simples relacionados con el Producto Interno Bruto (PIB) a nivel mundial y regional. El objetivo principal es demostrar cómo se puede crear un tablero de control simple y efectivo utilizando Python y generando visualizaciones con Plotly. [Ver repositorio](https://github.com/fowardelcac/PBI)
     
+    - **Análisis de Sentimiento**: Este proyecto se centró en realizar un análisis de sentimiento en relación a cinco prominentes políticos argentinos. Se basó en la evaluación de tweets en los cuales los usuarios los mencionaban. El proyecto incluye la extracción de tweets, procesamiento de datos y el uso de un modelo de Procesamiento de Lenguaje Natural (NLP) llamado roBERTa para generar análisis de sentimiento. [Ver repositorio](https://github.com/fowardelcac/Analisis-de-sentimiento-politicos-argentinos)
     
+    - **Análisis de Direcciones de Ethereum**: Exploré el emocionante mundo de las criptomonedas, conectándome a la API de Etherscan para crear una aplicación interactiva. Esta aplicación permite obtener datos a partir de las direcciones de Ethereum, incluyendo saldos, historiales de transacciones y tokens en posesión. [Ver repositorio](https://github.com/fowardelcac/Analisis-ethereum-address)
+    ''')
+    
+    st.subheader("Cómo contactarme:")
+    st.markdown('''
+    - **LinkedIn**: [Perfil de LinkedIn](https://www.linkedin.com/in/jcs2)
+    - **GitHub**: [Perfil de GitHub](https://github.com/fowardelcac)
+    - **Correo Electrónico**: juansaldano01@gmail.com
+    - **WhatsApp**: +54 351 512 6823
+    ''')
+        
     image_url = 'https://raw.githubusercontent.com/fowardelcac/Portafolio_Ciencia_de_Datos/main/70445613.jpg'
     response = requests.get(image_url)
     image = Image.open(BytesIO(response.content))
