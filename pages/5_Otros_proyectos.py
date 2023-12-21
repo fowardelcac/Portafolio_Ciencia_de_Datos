8f86bc97bf2c014f79290dd3b0849ed6fa238ee8
Juan Cruz Saldaño <70445613+fowardelcac@users.noreply.github.com>
2023-12-21 00:00:20 -0300

    Update 5_Otros_proyectos.py

--- pages/5_Otros_proyectos.py
+++ pages/5_Otros_proyectos.py
@@ -2,6 +2,12 @@ import streamlit as st
 
 st.title("Otros proyectos:")
 
+st.subheader('Scraping:')
+st.markdown('Scraping de una pagina web a traves de libreria Selenium')
+st.markdown('Codigo: https://github.com/fowardelcac/scraping_barcelona/blob/main/scraper.ipynb')
+st.markdown('Archivo csv: https://github.com/fowardelcac/scraping_barcelona/blob/main/MWCBarcelona.csv')
+st.write('-'*100)
+
 st.subheader('ML con SKlearn:')
 st.markdown('Implementacion de algoritmos de apredizaje supervizado/no superv., division de datasets e hyperparametrizacion utilizando la liberia Scikit learn')
 st.markdown('Link: https://github.com/fowardelcac/Machine-learning-con-sklearn')
